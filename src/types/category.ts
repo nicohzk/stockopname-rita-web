8caaf5bb52a23269b6eb7b035e3edad70dd46b6e
F3700 <albertfebrian26@gmail.com>
2026-09-25 02:22:38 +0700

    chore: remove category/department master modules

--- src/types/category.ts
+++
@@ -1,9 +0,0 @@
-export type Category = {
-  id: number;
-  name: string;
-  description: string;
-};
-
-export type CategoryRequest = { name: string; description: string };
-
-export type CategoryUpdateRequest = Partial<CategoryRequest>;
